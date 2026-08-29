0f39130e0752cfba675c410204d5dd92d4d213c3
willplacetech <wilquer.figueiredo@placetech.com.br>
2026-08-29 10:19:30 -0300

    capacitor e server

---
+++ android/app/src/main/java/com/meucaminho/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.meucaminho.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api' || 'https://meucaminhoback.onrender.com'
+  baseURL: import.meta.env.VITE_API_URL ||  'https://meucaminhoback.onrender.com'
 });
 
 api.interceptors.request.use(config => {
